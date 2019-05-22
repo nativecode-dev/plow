00e2ab9601db7f72598c58e3b5590bb333e326a3
Mike Pham <mike.pham@proplogix.com>
2019-05-22 00:33:46 -0400

    fixing heptio binary

--- env-gitlab.sh
+++ env-gitlab.sh
@@ -3,7 +3,7 @@
 export REGION=us-east-1
 export CLUSTER_NAME="${ENVIRONMENT}-gitlab"
 export CLUSTER_VERSION=1.12
-export MACHINE_TYPE=t2.small
+export MACHINE_TYPE=t2.medium
 export NUM_NODES=2
 export SERVICE_ACCOUNT=tiller
 
