c22ad4161e44d59501ab263e6cb286c3f2a26cc6
Mike Pham <mike.pham@proplogix.com>
2019-05-22 02:43:42 -0400

    adding helm and binary checks

--- env-gitlab.sh
+++ env-gitlab.sh
@@ -8,4 +8,29 @@ export NUM_NODES=2
 export SERVICE_ACCOUNT=tiller
 
 cd gitlab
+
+if [ "$ENVIRONMENT_ACTION" = "up" ]; then
+    read -p "Deploy EKS Cluster: [Y/N]: " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        exit 1
+    fi
+fi
+
 $BASH ./scripts/eks_bootstrap_script ${ENVIRONMENT_ACTION}
+
+if [ "$ENVIRONMENT_ACTION" = "up" ]; then
+    read -p "Deploy GitLab: [Y/N]: " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        exit 1
+    fi
+
+    $HELM repo add gitlab https://charts.gitlab.io/
+    $HELM repo update
+    $HELM upgrade --install gitlab gitlab/gitlab
+        \ --set certmanager-issuer.email=admin@${ENVIRONMENT_DOMAIN}
+        \ --set global.hosts.domain=${ENVIRONMENT_URL}
+        \ --set global.hosts.externalIP=${ENVIRONMENT_CLUSTER_IP}
+        \ --set global.hosts.ssh=${ENVIRONMENT_URL}
+        \ --set global.registry.bucket=${ENVIRONMENT_BUCKET}
+        \ --timeout 600
+fi
